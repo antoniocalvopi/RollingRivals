4ea2692fa362c3e48d3baf12590621461316a94f
Antonio Calvo <acalvopi@alumnos.unex.es>
2024-10-29 22:20:41 +0100

    download, estadios, compamentos y ligas

--- js/contador.js
+++ js/contador.js
@@ -1,5 +1,7 @@
-// Inicializar el contador local
-let localCount = localStorage.getItem('localCount') ? parseInt(localStorage.getItem('localCount')) : 0;
-localCount++; // Incrementar el contador local
-localStorage.setItem('localCount', localCount); // Guardar el nuevo valor en localStorage
-document.getElementById('localCount').textContent = localCount; // Actualizar el contador en la página
+function localStorage() {
+    // Inicializar el contador local
+    let localCount = localStorage.getItem('localCount') ? parseInt(localStorage.getItem('localCount')) : 0;
+    localCount++; // Incrementar el contador local
+    localStorage.setItem('localCount', localCount); // Guardar el nuevo valor en localStorage
+    document.getElementById('localCount').textContent = localCount; // Actualizar el contador en la página
+}
--- js/fetch.js
+++ js/fetch.js
@@ -1,3 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    loadHtml('/modules/header_2.html', 'header');
+    //loadHtml('/modules/header.html', 'header');
+    loadHtml('/modules/footer.html','footer');
+    highlightActivePage();
+});
+
 export function loadHtml(url, elementId) {
     fetch(url)
         .then(response => {
@@ -8,19 +15,22 @@ export function loadHtml(url, elementId) {
         })
         .then(data => {
             document.getElementById(elementId).innerHTML = data;
-            highlightActivePage(); //actualizar menu
         })
         .catch(error => console.error(`Error al cargar ${elementId}:`, error));
 }
 
 function highlightActivePage() {
     const currentPath = window.location.pathname;  // Obtiene la ruta actual
-    const menuLinks = document.querySelectorAll('nav ul li a');  // Selecciona todos los enlaces del menú
+    const menuLinks = document.querySelectorAll('.cs-li-link');  // Selecciona todos los enlaces del menú usando la nueva clase
 
     menuLinks.forEach(link => {
         if (link.getAttribute('href') === currentPath) {
-            // Agrega la clase 'selected' al elemento padre <li> del enlace activo
-            link.parentElement.classList.add('selected');
+            // Verifica si el enlace está dentro de una sublista o no
+            const listItem = link.closest('.cs-li');  // Encuentra el <li> más cercano
+
+            if (listItem) {
+                listItem.classList.add('cs-active');  // Agrega la clase al <li> que contiene el enlace
+            }
         }
     });
 }
