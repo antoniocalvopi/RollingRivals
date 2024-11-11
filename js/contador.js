21314de5ad6752714f9f5e5067f99fc143c5e189
Antonio Calvo <acalvopi@alumnos.unex.es>
2024-11-11 11:58:39 +0100

    update

--- js/contador.js
+++ js/contador.js
@@ -1,7 +1,6 @@
-function localStorage() {
-    // Inicializar el contador local
-    let localCount = localStorage.getItem('localCount') ? parseInt(localStorage.getItem('localCount')) : 0;
-    localCount++; // Incrementar el contador local
-    localStorage.setItem('localCount', localCount); // Guardar el nuevo valor en localStorage
-    document.getElementById('localCount').textContent = localCount; // Actualizar el contador en la página
-}
+// Inicializar el contador local
+let localCount = localStorage.getItem('localCount') ? parseInt(localStorage.getItem('localCount')) : 0;
+localCount++; // Incrementar el contador local
+localStorage.setItem('localCount', localCount); // Guardar el nuevo valor en localStorage
+document.getElementById('localCount').textContent = localCount; // Actualizar el contador en la página
+
--- js/fetch.js
+++ js/fetch.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    loadHtml('/modules/header.html', 'header');
+    // loadHtml('/modules/header.html', 'header');
     loadHtml('/modules/footer.html','footer');
 });
 
@@ -15,28 +15,4 @@ function loadHtml(url, elementId) {
             document.getElementById(elementId).innerHTML = data;
         })
         .catch(error => console.error(`Error al cargar ${elementId}:`, error));
-}
-
-function highlightActivePage() {
-    const currentPath = window.location.pathname;  // Obtiene la ruta actual
-    const menuLinks = document.querySelectorAll('.cs-li-link');  // Selecciona todos los enlaces del menú usando la clase
-
-    menuLinks.forEach(link => {
-        if (link.getAttribute('href') === currentPath) {
-            // Verifica que el enlace esté dentro de un <li>
-            const listItem = link.closest('.cs-li');  // Encuentra el <li> más cercano
-
-            if (listItem) {
-                // Si deseas agregar la clase al enlace directamente:
-                link.classList.add('cs-selected');  // Agrega la clase 'cs-selected' al enlace (.cs-li-link)
-                console.log(`Se ha agregado la clase 'cs-selected' al enlace: ${link.href}`);
-
-                // Si también deseas agregar la clase al <li> para indicar el estado del contenedor:
-                listItem.classList.add('cs-selected');  // Agrega la clase 'cs-selected' al contenedor <li>
-                console.log(`Se ha agregado la clase 'cs-selected' al contenedor <li>: ${listItem}`);
-            } else {
-                console.warn(`No se encontró el <li> para el enlace: ${link.href}`);
-            }
-        }
-    });
 }
