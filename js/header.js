5275bf8f93c9db94068a13b913122a1079c549de
Antonio Calvo <acalvopi@alumnos.unex.es>
2024-11-16 20:55:41 +0100

    dropdown funcional en desktop y movil

--- js/header.js
+++ js/header.js
@@ -1,10 +1,9 @@
-// Selección de elementos necesarios
 const CSbody = document.querySelector("body");
 const CSnavbarMenu = document.querySelector("#cs-navigation");
 const CShamburgerMenu = document.querySelector("#cs-navigation .cs-toggle");
 const csUL = document.querySelector('#cs-expanded');
 
-// Función para alternar el menú móvil y cambiar el estado de aria-expanded
+// alternar el menú móvil y cambiar el estado de aria-expanded
 CShamburgerMenu.addEventListener('click', () => {
     const isActive = CShamburgerMenu.classList.toggle("cs-active");
     CSnavbarMenu.classList.toggle("cs-active");
@@ -12,21 +11,29 @@ CShamburgerMenu.addEventListener('click', () => {
     csUL.setAttribute('aria-expanded', isActive ? 'true' : 'false');
 });
 
-// Código para manejar el dropdown en dispositivos móviles
-const dropDowns = document.querySelectorAll('#cs-navigation .cs-dropdown');
+// dropdown
 
-dropDowns.forEach(item => {
-    item.addEventListener('click', (event) => {
-        // Evita que el enlace redireccione cuando se hace clic
+const isMobile = window.matchMedia("(max-width: 768px)").matches;
+const dropdown = document.querySelector('.cs-dropdown');
+const dropdownLink = dropdown.querySelector('.cs-li-link');
+const dropdownMenu = dropdown.querySelector('.cs-drop-ul'); 
+// Abrir o cerrar el dropdown al hacer clic en el enlace ("Actividades")
+dropdownLink.addEventListener('click', function (event) {
+    if (isMobile) {
         event.preventDefault();
-        // Alterna la clase activa en el dropdown actual
-        item.classList.toggle('cs-active');
-    });
-
-    // Permite la apertura del dropdown con teclado
-    item.addEventListener('keypress', (event) => {
-        if (event.key === 'Enter') {
-            item.classList.toggle('cs-active');
-        }
-    });
+        dropdown.classList.toggle('cs-active'); 
+        dropdownMenu.classList.toggle('show'); 
+    }
+});
+// Cerrar el dropdown si se hace clic fuera del menú
+document.addEventListener('click', function (event) {
+    if (!dropdown.contains(event.target)) {
+        dropdown.classList.remove('cs-active');
+        dropdownMenu.classList.remove('show');
+    }
+});
+// Permitir la redirección cuando se haga clic en los enlaces dentro del dropdown
+const dropdownItems = dropdown.querySelectorAll('.cs-drop-link');
+dropdownItems.forEach(function (item) {
+    item.addEventListener('click', function (event) {});
 });
