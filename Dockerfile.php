cbb50a6dc5d98f08c1d4dca15a089aff2dbc5688
antoniocalvopi <acalvopi@alumnos.unex.es>
2025-01-24 12:46:43 +0100

    ejemplo dockerfiles para despliegue en docker

---
+++ Dockerfile.php
@@ -0,0 +1,13 @@
+FROM php:8.2-cli
+
+# Establece el directorio de trabajo
+WORKDIR /app
+
+# Copia los archivos necesarios
+COPY /py_server/ /app/
+
+# Exponer el puerto 3000 para el servidor PHP
+EXPOSE 3000
+
+# Comando para iniciar el servidor PHP
+CMD ["php", "-S", "0.0.0.0:3000"]
